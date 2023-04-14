b5044a3481a9b718ccd76b035476894c0ae880bd
nonamekairat <113531357+nonamekairat@users.noreply.github.com>
2023-04-14 14:23:33 +0600

    removed commad in userController request mapping

--- src/main/java/com/example/LaptopKG/controller/UserController.java
+++ src/main/java/com/example/LaptopKG/controller/UserController.java
@@ -7,13 +7,15 @@ import com.example.LaptopKG.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/")
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserService userService;
