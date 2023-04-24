dcabbc3b0fad8b4f24de5a2690316f700d16329e
Diyor Umurzakov <diyorp2@mail.ru>
2023-04-24 10:11:14 +0600

    Updated enums getting

--- src/main/java/com/example/LaptopKG/controller/EnumController.java
+++ src/main/java/com/example/LaptopKG/controller/EnumController.java
@@ -2,6 +2,10 @@ package com.example.LaptopKG.controller;
 
 
 import com.example.LaptopKG.service.EnumService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -12,14 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 @RequestMapping("/api/types")
+@Tag(
+        name = "Контроллер для работы с enums",
+        description = "В этом контроллеры есть возможности получения всех enum'ов"
+)
 public class EnumController {
-
     private final EnumService enumService;
 
     @GetMapping("/{type}")
-    public ResponseEntity<?> getCategories(@PathVariable String type){
+    @Operation(
+            summary = "Получение enum'ов по типу"
+    )
+    public ResponseEntity<?> getByType(@PathVariable
+                                       @Parameter(description = "(category, delivery-type, guarantee, " +
+                                               "hardware-type, order-status, payment-type, status)")
+                                           String type){
         return ResponseEntity.ok(enumService.getListByType(type));
     }
 
